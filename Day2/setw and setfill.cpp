d716f3d7fdab7fcdb08711ebb7f0c37b5370dabd
Amit Patnaik <77204104+Amit-45@users.noreply.github.com>
2021-05-30 21:35:30 +0530

    Create setw and setfill.cpp

---
+++ Day2/setw and setfill.cpp
@@ -0,0 +1,15 @@
+//Pattern printing program using setw and setfill
+#include <iostream>
+#include<iomanip>
+using namespace std;
+int main()
+{
+    int n = 4;
+
+    for (int i = 1; i <= n; i++)
+    {
+        cout << std::left << std::setfill(' ') << std::setw(n);
+        cout << std::string(i, '*')<<"\n";
+      
+    }
+}

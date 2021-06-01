478763a0986a82fe8398bcb2470feafea2c44db4
Amit Patnaik <77204104+Amit-45@users.noreply.github.com>
2021-06-01 20:52:39 +0530

    Create pow and log.cpp

---
+++ Day4/pow and log.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <cmath> //header file to use library functions
+using namespace std;
+int main()
+{
+
+    double num1 = 2, num2 = 4, num3 = 100;
+
+    //power function
+    cout << pow(num1, num2) << endl;
+
+    //log function
+    cout << log10(num3);
+
+    return 0;
+}
+/*
+
+Output:
+16
+2
+
+*/

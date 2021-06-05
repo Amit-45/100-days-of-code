2a8caa3edc22a210f9dfd765c8ddc404831ea622
Amit Patnaik <77204104+Amit-45@users.noreply.github.com>
2021-06-05 21:21:42 +0530

    Create check element in array.cpp

---
+++ Day8/check element in array.cpp
@@ -0,0 +1,45 @@
+/*
+Problem - Check	whether	m	is	present	in	an	array	of	size	n	or	not.	
+Input	-	n,m	(Size of array,	number to be searched )
+      -	Take	input	n	elements	for	the	array
+Output	->	true/false
+[Hint:-	Create a dynamic array]	
+
+*/
+#include <bits/stdc++.h>
+using namespace std;
+bool checkElement(int arr[],int n,int m)
+{
+    int flag=0;
+    for(int i=0;i<n;i++)
+    {
+        if (arr[i]==m)
+        {
+            flag=1;
+        }
+    }
+    return flag;
+}
+int main()
+{
+    int n,m;
+    cin >> n>>m;
+    
+    int arr[n]; //dynamic array
+    
+    for(int i=0;i<n;i++) cin>>arr[i];   
+    
+    cout<<std::boolalpha<<checkElement(arr,n,m);
+     
+    return 0;
+}
+/* 
+
+Input:
+3 2
+1 2 2 4
+
+Output:
+true
+
+*/

e00c82d464c810a36c3256a4421de817358bc2d9
Amit Patnaik <77204104+Amit-45@users.noreply.github.com>
2021-06-08 20:59:24 +0530

    Create address of variable.cpp

---
+++ Day-11/address of variable.cpp
@@ -0,0 +1,10 @@
+//Write a program to print the address of a variable whose value is input from user.
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+  int x;
+  cin>>x;
+  
+  cout<<&x<<endl;
+}

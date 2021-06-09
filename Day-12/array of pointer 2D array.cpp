761230d818ece085e6b878f9ef54c9a3b4391254
Amit Patnaik <77204104+Amit-45@users.noreply.github.com>
2021-06-09 20:07:25 +0530

    Create array of pointer 2D array.cpp

---
+++ Day-12/array of pointer 2D array.cpp
@@ -0,0 +1,20 @@
+//Program to create multidimentional array using array of pointer method and then printing it.
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+  int m=3,n=2;
+  //Creating m*n array  
+   int *arr[m];  
+  for(int i=0;i<m;i++)
+    arr[i]=new int[n];
+  
+  
+  for(int i=0;i<m;i++) 
+    for(int j=0;j<n;j++)
+    {
+      arr[i][j]=5;
+       cout<<arr[i][j]<<" "; //Printing array
+    } 
+}
+//These arrays are allocated in stack

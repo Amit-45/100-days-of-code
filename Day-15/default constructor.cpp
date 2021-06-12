f95ca34ed09607da955ffe3ce2d725bd936a4856
Amit Patnaik <77204104+Amit-45@users.noreply.github.com>
2021-06-12 21:40:10 +0530

    Create default constructor.cpp

---
+++ Day-15/default constructor.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>  
+using namespace std;  
+class Employee  
+ {  
+   public:  
+        Student()    
+        {    
+            cout<<"---Default Constructor---"<<endl;    
+        }    
+};  
+int main(void)   
+{  
+    Student s1; 
+    Student s2;
+ return 0;  
+}  

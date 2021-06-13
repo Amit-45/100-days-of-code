5060739aa55f7d9fcad2fca53e927cf517a7bc9c
Amit Patnaik <77204104+Amit-45@users.noreply.github.com>
2021-06-13 20:18:43 +0530

    Create single inheritence.cpp

---
+++ Day-16/single inheritence.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>  
+using namespace std;  
+class A
+ {  
+  public :  
+        void func()
+        {
+            cout<<"Inherited";
+        }
+};  
+class B : public A
+{
+    
+};
+int main() {
+    B b;
+    b.func();
+
+    return 0;
+}
+//Output : Inherited 

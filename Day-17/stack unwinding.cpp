480c51c89ca16795c8d1ff18c9e4493c81692d28
Amit Patnaik <77204104+Amit-45@users.noreply.github.com>
2021-06-14 10:24:28 +0530

    Create stack unwinding.cpp

---
+++ Day-17/stack unwinding.cpp
@@ -0,0 +1,40 @@
+//exception handling involves Stack Unwinding if exception is not handled in same function (where it is thrown).
+#include <iostream>
+using namespace std;
+  
+void f1() throw (int) {
+  cout<<"f1 Begins\n";
+  throw 100;
+  cout<<" f1 Ends\n"; 
+}
+  
+void f2() throw (int) {
+  cout<<"f2 Begins\n";
+  f1();
+  cout<<"f2 Ends\n";
+}
+   
+void f3() {
+  cout<<"f3 Begins\n";
+  
+  cout<<"f3 Ends\n";
+}
+  
+
+int main() {
+  try {
+    f2();
+  }
+  catch(int i) {
+   cout<<"Caught Exception\n";
+  }
+  cout<<"Bye!!";
+} 
+/*
+
+f2 Begins
+f1 Begins
+Caught Exception
+Bye!!
+
+*/

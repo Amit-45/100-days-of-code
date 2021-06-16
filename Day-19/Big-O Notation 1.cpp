e7a805f75fa7ae123aa5a53285d725cb2256cc1c
Amit Patnaik <77204104+Amit-45@users.noreply.github.com>
2021-06-16 22:15:36 +0530

    Create Big-O Notation 1.cpp

---
+++ Day-19/Big-O Notation 1.cpp
@@ -0,0 +1,14 @@
+//Program to print the first array element
+#include<bits/stdc++.h>
+using namespace std;
+
+void printFirstElementOfArray(int arr[])
+{
+   cout<<arr[0]<<endl;
+}
+int main()
+{
+    int arr[]={1,2,3,4,5};
+    printFirstElementOfArray(arr);
+}
+//Time complexity : O(1)  or Constant Time
